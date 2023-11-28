cf1e08960c0327436a256bce373329914ae4fccc
Jhedie <jeddiahawuku12@gmail.com>
2023-11-28 11:26:33 +0000

    chore: Set up front end app with init files

---
+++ Frontend/babel.config.js
@@ -0,0 +1,38 @@
+process.env.TAMAGUI_TARGET = "native"; // Don't forget to specify your TAMAGUI_TARGET here
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [["babel-preset-expo", { jsxRuntime: "automatic" }]],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true
+        }
+      ],
+      [
+        "transform-inline-environment-variables",
+        // NOTE: include is optional, you can leave this part out
+        {
+          include: ["TAMAGUI_TARGET", "EXPO_ROUTER_APP_ROOT"]
+        }
+      ],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true
+        }
+      ],
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel")
+    ]
+  };
+};

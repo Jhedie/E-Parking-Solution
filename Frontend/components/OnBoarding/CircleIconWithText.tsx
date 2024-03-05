0148b32a7c52ad8637079b089e306911f8b6fd56
jea22 <jea22@student.le.ac.uk>
2024-03-05 18:35:51 +0000

    fix: Onboarding navigation

--- Frontend/components/OnBoarding/CircleIconWithText.tsx
+++ Frontend/components/OnBoarding/CircleIconWithText.tsx
@@ -1,72 +1,72 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import MapIcon from '@assets/static/svgs/map-icon.svg'; 
-
+import MapIcon from "@assets/static/svgs/map-icon.svg";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 // Define the types for your props
 interface CircleIconWithTextProps {
-    title: string;
-    showIcon: boolean;
-    outerCircleWidth?: number;
-    outerCircleHeight?: number;
-    innerCircleWidth?: number;
-    innerCircleHeight?: number;
-    icon?: React.ComponentType<{ width: number, height: number }>; 
-  }
+  title: string;
+  showIcon: boolean;
+  outerCircleWidth?: number;
+  outerCircleHeight?: number;
+  innerCircleWidth?: number;
+  innerCircleHeight?: number;
+  icon?: React.ComponentType<{ width: number; height: number }>;
+}
 
-const CircleIconWithText: React.FC<CircleIconWithTextProps> = ({ 
-    title, 
+const CircleIconWithText: React.FC<CircleIconWithTextProps> = ({
+  title,
 
-    showIcon,
-    icon: Icon,
+  showIcon,
+  icon: Icon,
 
-    outerCircleWidth,
-    outerCircleHeight,
-    innerCircleWidth,
-    innerCircleHeight,
+  outerCircleWidth,
+  outerCircleHeight,
+  innerCircleWidth,
+  innerCircleHeight
 }) => {
   return (
     <View className="px-8 flex flex-col items-center justify-center mx-auto">
-        
-    {showIcon && (
+      {showIcon && (
         <View className="icon-container mb-3">
-            <View 
+          <View
             style={{
-                ...styles.circleStyles, 
-                height: outerCircleHeight ? outerCircleHeight : 45,
-                width: outerCircleWidth ? outerCircleWidth : 45
-            }} 
-            className='outer-circle'>
-              <View 
+              ...styles.circleStyles,
+              height: outerCircleHeight ? outerCircleHeight : 45,
+              width: outerCircleWidth ? outerCircleWidth : 45
+            }}
+            className="outer-circle"
+          >
+            <View
               style={{
-                  ...styles.circleStyles, 
-                  height: innerCircleHeight ? innerCircleHeight : 33,
-                  width: innerCircleWidth ? innerCircleWidth : 33
-              }} 
-              className='inner-circle'
-              >
-                  <Icon width={15} height={15} />
-              </View>
+                ...styles.circleStyles,
+                height: innerCircleHeight ? innerCircleHeight : 33,
+                width: innerCircleWidth ? innerCircleWidth : 33
+              }}
+              className="inner-circle"
+            >
+              <MapIcon
+                width={15}
+                height={15}
+              />
             </View>
+          </View>
         </View>
-    )};
-      <Text className="text-lg font-bold text-dark head">
-            {title}
-      </Text>
+      )}
+      ;<Text className="text-lg font-bold text-dark head">{title}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   iconContainer: {
-    marginBottom: 3,
+    marginBottom: 3
   },
   circleStyles: {
     // determine outcircle width and heigh from user or make default 45
     borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
 export default CircleIconWithText;

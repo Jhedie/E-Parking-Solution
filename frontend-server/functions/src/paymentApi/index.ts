f7b8b3e0e9ad4f7e612c1288a3305df3fab56a07
jea22 <jea22@student.le.ac.uk>
2024-04-26 10:01:14 +0100

    refactor: Refine payment api

--- frontend-server/functions/src/paymentApi/index.ts
+++ frontend-server/functions/src/paymentApi/index.ts
@@ -1,13 +1,13 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
+// import * as admin from "firebase-admin";
+import Stripe from "stripe";
 
 dotenv.config();
 
 const paymentApp: Express = express();
-
-paymentApp.use(express.json());
-const corsOptions = {
+const corsOptions: cors.CorsOptions = {
   origin: "*", // Allow only this origin to access
   methods: "GET,POST,PUT,DELETE,OPTIONS", // Allowed methods
   allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization", // Allowed headers
@@ -15,54 +15,147 @@ const corsOptions = {
 };
 
 paymentApp.use(cors(corsOptions));
-paymentApp.get("/payment/hello", (req, res) => {
+paymentApp.get("/payment/hello", (req: Request, res: Response) => {
   res.send("Payment API Deployed and Working!");
 });
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-// This example sets up an endpoint using the Express framework.
-// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2023-10-16",
+});
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET;
 
-paymentApp.post("/payment-sheet", async (req, res) => {
-  //  body: JSON.stringify({
-  //         amount: route.params.bookingDetails.totalprice, // Amount in the smallest currency unit (e.g., cents for USD)
-  //         currency: "gbp",
-  //         description:
-  //           "Payment for parking - " +
-  //           ConfirmationDetails.parkingLot.LotId +
-  //           " - " +
-  //           ConfirmationDetails.parkingSlot.Position.Row +
-  //           ConfirmationDetails.parkingSlot.Position.Column,
-  //         customer: {
-  //           name: `${user?.displayName}`,
-  //           email: `${user?.email}`
-  //         }
-  // get the detauls from the body of the request
-  console.log(req.body);
-  const { amount, currency } = req.body;
-  // Use an existing Customer ID if this is a returning customer.
+paymentApp.use(
+  (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): void => {
+    if (req.originalUrl === "/webhook") {
+      next();
+    } else {
+      express.json()(req, res, next);
+    }
+  }
+);
 
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    { customer: "cus_PowIhjjShxeRnf" },
-    { apiVersion: "2023-10-16" }
-  );
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: currency,
-    customer: "cus_PowIhjjShxeRnf",
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-    // is optional because Stripe enables its functionality by default.
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+paymentApp.post(
+  "/webhook",
+  // Stripe requires the raw body to construct the event
+  express.raw({ type: "application/json" }),
+  (req: express.Request, res: express.Response): void => {
+    const sig = req.headers["stripe-signature"];
 
-  res.json({
-    paymentIntent: paymentIntent.client_secret,
-    ephemeralKey: ephemeralKey.secret,
-    customer: "cus_PowIhjjShxeRnf",
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err: any) {
+      // On error, log and return the error message
+      console.log(`❌ Error message: ${err.message}`);
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Successfully constructed event
+    console.log("✅ Success:", event.id);
+
+    // Cast event data to Stripe object
+    if (event.type === "payment_intent.succeeded") {
+      const stripeObject: Stripe.PaymentIntent = event.data
+        .object as Stripe.PaymentIntent;
+      console.log(`💰 PaymentIntent status: ${stripeObject.status}`);
+    } else if (event.type === "charge.succeeded") {
+      const charge = event.data.object as Stripe.Charge;
+      console.log(`💵 Charge id: ${charge.id}`);
+    } else {
+      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event
+    res.json({ received: true });
+  }
+);
+
+paymentApp.post("/payment-sheet", async (req: Request, res: Response) => {
+  try {
+    const { amount, currency, description, customerDetails, parkingDetails } =
+      req.body;
+
+    const customer = await stripe.customers.create();
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: "2023-10-16" }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: currency,
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      metadata: {
+        description: description,
+        driverId: customerDetails.id,
+        name: customerDetails.name,
+        email: customerDetails.email,
+        phone: customerDetails.phone,
+        parkingLotId: parkingDetails.lotId,
+        parkingSlotId: parkingDetails.slotId,
+        bookingId: parkingDetails.bookingId,
+      },
+    });
+
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+paymentApp.post("/extend-booking", async (req: Request, res: Response) => {
+  const { amount, customer, customerDetails, parkingDetails } = req.body;
+  console.log("Amount: ", amount);
+  console.log("Customer: ", customer);
+  console.log("Customer Details: ", customerDetails);
+  console.log("Parking Details: ", parkingDetails);
+
+  if (amount > 0) {
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer },
+      { apiVersion: "2023-10-16" } // Ensure this matches the API version used by your client
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "gbp",
+      customer: customer,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+      metadata: {
+        driverId: customerDetails.id,
+        name: customerDetails.name,
+        email: customerDetails.email,
+        phone: customerDetails.phone,
+        parkingLotId: parkingDetails.lotId,
+        parkingSlotId: parkingDetails.slotId,
+        bookingId: parkingDetails.bookingId,
+      },
+    });
+
+    res.json({
+      success: true,
+      message: "Booking extended successfully",
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+  }
 });
 
 export { paymentApp };
